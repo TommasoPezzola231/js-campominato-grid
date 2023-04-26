68de792d3c6b5c7059ebe5620febfe523268329f
TommasoPezzola <pezzola123@gmail.com>
2023-04-26 20:03:23 +0200

    add Bonus

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
 function creaSquare(number) {
     for (let i = 1; i <= number; i++) {
         let square = `<div class="square">${i}</div>`;
+        
         box.innerHTML += square
-        console.log("Box creato!")
+        console.log("Box creato!");
+        
     }
     
 }
 
 const box = document.getElementById("box");
 const play = document.getElementById("start");
-let value = 100;
+let mode = document.getElementById("values");
 
 
 play.addEventListener("click", function () {
     console.log("Play!");
-    creaSquare(value)
+    creaSquare(mode.value)
     let square = document.getElementsByClassName("square");
     
-    for (let c = 0; c < value; c++) {
+    
+    for (let c = 0; c < mode.value; c++) {
+        
+        if (mode.value == 49) {
+            square[c].classList.add(`easy`)
+        }
+        
+        if (mode.value == 81) {
+            square[c].classList.add("medium")
+        }
         
         square[c].addEventListener("click", function () {
             this.classList.toggle("clicked")
